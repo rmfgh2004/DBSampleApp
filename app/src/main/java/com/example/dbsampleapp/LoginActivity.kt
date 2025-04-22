8cf984c4600a92f49709266dbe055cae25d563eb
rmfgh2004 <rmfgh2004@naver.com>
2025-04-22 10:22:57 +0000

    login setting

--- app/src/main/java/com/example/dbsampleapp/LoginActivity.kt
+++ app/src/main/java/com/example/dbsampleapp/LoginActivity.kt
@@ -7,25 +7,37 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.dbsampleapp.databinding.ActivityLoginBinding
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.loginButton.setOnClickListener { login() }
     }
 
-    companion object {
-        fun start(context: Context) {
-            val intent = Intent(context, MainActivity::class.java).apply {
-                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-            }
-            context.startActivity(intent)
-        }
+    // login logic
+    private fun login() {
+        saveInformation()
+        MainActivity.start(this)
     }
+
+    // SharedPreference (id and pw) save
+    private fun saveInformation() {
+
+    }
+
+
 }
